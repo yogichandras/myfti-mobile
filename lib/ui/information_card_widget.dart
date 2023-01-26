204e052eadb3b13da8f5e131640b2b8e29f23e70
Rachmizard <rachmizard11072000@gmail.com>
2023-01-27 00:08:32 +0700

    feat(): add modal bottom sheet on tap information card widget

--- lib/ui/information_card_widget.dart
+++ lib/ui/information_card_widget.dart
@@ -12,37 +12,89 @@ class InformationCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 0,
-      color: primaryColor.withOpacity(0.9),
+      color: primaryColor.withOpacity(0.8),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Padding(
-        padding: const EdgeInsets.only(
-          bottom: 20,
-          top: 10,
-          left: 20,
-          right: 20,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(title,
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold)),
-              const SizedBox(
-                height: 10,
+      child: InkWell(
+        onTap: () {
+          showModalBottomSheet<void>(
+            context: context,
+            enableDrag: true,
+            isScrollControlled: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(20),
               ),
-              Text(
-                paragraph,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
+            ),
+            builder: (BuildContext context) {
+              return DraggableScrollableSheet(
+                expand: false,
+                builder: (context, scrollController) {
+                  return Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(title,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold)),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              paragraph,
+                              style: TextStyle(
+                                color: secondaryColor,
+                                fontSize: 16,
+                              ),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(
+            bottom: 20,
+            top: 10,
+            left: 20,
+            right: 20,
+          ),
+          child: SingleChildScrollView(
+            clipBehavior: Clip.hardEdge,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 10,
                 ),
-              )
-            ],
+                Text(
+                  paragraph,
+                  maxLines: 6,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
