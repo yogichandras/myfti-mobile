8e3e10d46e813c7d309cc155829b14111144bb30
Rachmizard <rachmizard11072000@gmail.com>
2023-01-26 21:56:34 +0700

    fix(): router conflict

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:myfti/screens/sample_ui_screen.dart';
-
-import '../screens/home_screen.dart';
-import '../screens/profile_screen.dart';
-import '../screens/schedule_screen.dart';
+import 'package:myfti/screens/login_screen.dart';
+import 'package:myfti/screens/register_screen.dart';
+import 'package:myfti/screens/root_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  // since apps grow, we need to get rid of 'SampleUIWidgets'
-  '/': (context) => const SampleUIWidgets(),
-  // define your routes here
-  // '/login': (context) => const LoginScreen(),
-  // '/register': (context) => const RegisterScreen(),
-
-  // task figur
-  // Home Screen
-  // Profile Screen
-  // Schedule Screen
-  '/home': (context) => const HomeScreenWidgets(),
-  '/profile': (context) => const ProfileScreen(),
-  '/schedule': (context) => const ScheduleScreen(),
-  // '/home': (context) => const HomeScreenWidgets(),
-  // '/profile': (context) => const ProfileScreen(),
-  // '/': (context) => const ScheduleScreen(),
+  '/login': (context) => const LoginScreen(),
+  '/register': (context) => const RegisterScreen(),
+  '/': (context) => const RootScreen(),
 };
